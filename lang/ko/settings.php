dd09ba6780ada02cfc5498d4bd5d8fa7114626e5
dryoo <dryoo@live.com>
2014-09-27 09:04:52 +0900

    Create settings.php

---
+++ lang/ko/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang["allowed_domains"] = '허용된 이메일 도메인';
+$lang["client_id"] = "구글API Client ID";
+//$lang["redirect_uri"] = "구글API Redirect URI";
+$lang["client_secret"] = "구글API Client Secret";
+$lang["default_groups"] = "기본그룹";
+
+?>
