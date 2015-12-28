d6f201ab592a3cc02a4636575f2820d5ce9a3c0a
ATMartin <martin.alex.t@gmail.com>
2015-12-28 03:20:23 -0500

    Add patch to redirect to desired page after login/reauth.

--- auth.php
+++ auth.php
@@ -67,6 +67,11 @@ class auth_plugin_authgoogle extends auth_plugin_authplain  {
             $_SESSION[DOKU_COOKIE]['authgoogle']['token'] = $_COOKIE[AUTHGOOGLE_COOKIE];
         }
 
+	//set our referer for redirection, if we're hitting login
+	if (!empty($_SERVER['HTTP_REFERER']) {
+		$_SESSION[DOKU_COOKIE]['authgoogle']['referer'] = $_SERVER['HTTP_REFERER'];
+	}
+
         //google auth
         require_once GOOGLE_API_DIR.'/Google_Client.php';
         require_once GOOGLE_API_DIR.'/contrib/Google_Oauth2Service.php';
@@ -169,9 +174,12 @@ class auth_plugin_authgoogle extends auth_plugin_authplain  {
             // update token
             $_SESSION['token'] = $client->getAccessToken();
 
-            //if login page - redirect to main page
-            if (isset($_GET['do']) && $_GET['do']=='login')
-                header("Location: ".wl('start', '', true));
+            //if login page - redirect to original referer or, if none, start page.
+            if (isset($_GET['do']) && $_GET['do']=='login') {
+		$referer = $_SESSION[DOKU_COOKIE]['authgoogle']['referer'];
+	    	$redirect = empty($referer) ? wl('start', '', true) : $referer;
+		header("Location: ".$referer);
+	    }
 
             return true;
         } else {
